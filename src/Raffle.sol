dc315491fb4844006fe710f82f0050a6fca3c8fb
AlanMachado <alanlm.1901@gmail.com>
2025-03-03 19:55:26 -0300

    adds enter raffle logic, creates events and errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,14 +8,29 @@ pragma solidity ^0.8.28;
  * @dev implements ChainLink VRFv2.5 
  */
 contract Raffle {
+    /**
+     * Errors
+     * Syntax [Contract_name]__[ErrorHandling]
+     */
+    error Raffle__InsufficientEntranceFee();
+
     uint256 private immutable i_entranceFee;
+    address payable [] private s_players;
+
+    event RaffleAccepted(address index player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
     function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, Raffle__InsufficientEntranceFee()); this is still less gas eficient than...
+        if (msg.value < i_entranceFee) {
+            revert Raffle__InsufficientEntranceFee();
+        }
+        s_players.push(payable(msg.sender));
 
+        emit RaffleAccepted(msg.sender);
     }
 
     function pickWinner() public {
