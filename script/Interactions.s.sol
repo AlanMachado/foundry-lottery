273a0d142bea9f788c3bf591b77cedd66db18b43
AlanMachado <alanlm.1901@gmail.com>
2025-04-07 09:51:15 -0300

    adds subscription id

---
+++ script/Interactions.s.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig, CodeConstants} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns(uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId,) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubscription(address vrfCoordinator) public returns(uint256, address) {
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+        console.log("Your subscription id is: ", subId);
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {
+        createSubscriptionUsingConfig();
+    }
+}
+
+contract FundSubscription is Script, CodeConstants {
+    uint256 public constant FUND_AMOUNT = 3 ether;
+    
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+        address link = helperConfig.getConfig().link;
+        fundSubscription(subscriptionId, vrfCoordinator, link);
+    }
+
+    function fundSubscription(uint256 subId, address vrfCoordinator, address link) public {
+        console.log("Funding subscription:", subId);
+        console.log("VRF Coordinator address: ", vrfCoordinator);
+        console.log("On Chain Id: ", block.chainid);
+
+        if (block.chainid == LOCAL_CHAIN_ID) {
+            vm.startBroadcast();
+            VRFCoordinatorV2_5Mock(vrfCoordinator).fundSubscription(subId, FUND_AMOUNT);
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(link).transferAndCall(vrfCoordinator, FUND_AMOUNT, abi.encode(subId));
+            vm.stopBroadcast();
+        }
+    }
+
+    function run() external {
+        fundSubscriptionUsingConfig();
+    }
+}
